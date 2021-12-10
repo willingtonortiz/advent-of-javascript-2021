25e1c8563e9adc597027c1ca8897ed8cb1a85d24
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2021-12-10 09:32:00 -0500

    Complete day-09

--- src/days/day-09/Day_09_Page.tsx
+++ src/days/day-09/Day_09_Page.tsx
@@ -1,5 +1,159 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import chevron from "./assets/images/chevron.svg";
+import { content } from "./assets/data/content";
+
+const CONTENT = content;
+
+const Body = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap");
+  font-family: "Roboto Mono", monospace;
+  padding: 0;
+  margin: 0;
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Feature = styled.div`
+  padding-bottom: 200px; /* offset thumbnail strip at the bottom */
+
+  & img {
+    max-height: 615px;
+    max-width: 800px;
+  }
+`;
+
+const Thumbnails = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+  overflow-x: scroll;
+  scroll-padding: 0 100px;
+  scroll-snap-type: both mandatory;
+
+  & ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    gap: 30px;
+    position: relative;
+    padding: 0 100px;
+  }
+
+  & li:last-child img {
+    margin-right: 100px;
+  }
+
+  & li {
+    scroll-snap-align: start;
+    scroll-snap-stop: normal;
+  }
+
+  & .selected {
+    outline: 10px solid #ff00c7;
+    outline-offset: -10px;
+  }
+
+  & img {
+    object-fit: cover;
+    height: 175px;
+    width: 175px;
+    vertical-align: middle;
+  }
+`;
+
+const CarouselButton = styled.button`
+  background: rgba(255, 0, 199, 0.3);
+  width: 100px;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 200px;
+  box-sizing: border-box;
+
+  &:hover {
+    background: rgba(255, 0, 199, 1);
+  }
+`;
+
+const CarouselLeft = styled(CarouselButton)`
+  left: 0;
+`;
+
+const CarouselRight = styled(CarouselButton)`
+  right: 0;
+
+  & img {
+    transform: rotate(180deg);
+    margin: auto;
+  }
+`;
+
 const Day_09_Page = () => {
-  return <h1>Day 09</h1>;
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedImage, setSelectedImage] = useState(CONTENT[0]);
+
+  useEffect(() => {
+    document.title = "AOJ 2021 - Day 9";
+  }, []);
+
+  useEffect(() => {
+    setSelectedImage(CONTENT[selectedIndex]);
+  }, [selectedIndex]);
+
+  const selectNextImage = () => {
+    if (selectedIndex + 1 >= CONTENT.length) {
+      return;
+    }
+
+    setSelectedIndex(selectedIndex + 1);
+  };
+  const selectPreviousImage = () => {
+    if (selectedIndex - 1 < 0) {
+      return;
+    }
+
+    setSelectedIndex(selectedIndex - 1);
+  };
+
+  return (
+    <Body>
+      <Feature>
+        <img src={selectedImage.image} alt="Featured" />
+
+        <div className="caption">{selectedImage.caption}</div>
+      </Feature>
+
+      <CarouselLeft onClick={() => selectPreviousImage()}>
+        <img src={chevron} alt="chevron" />
+      </CarouselLeft>
+
+      <CarouselRight onClick={() => selectNextImage()}>
+        <img src={chevron} alt="chevron" />
+      </CarouselRight>
+
+      <Thumbnails>
+        <ul>
+          {CONTENT.map((x, index) => (
+            <li key={index} onClick={() => setSelectedImage(x)}>
+              <a href="#">
+                <img src={x.image} alt={x.caption} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </Thumbnails>
+    </Body>
+  );
 };
 
 export default Day_09_Page;
---
+++ src/days/day-09/assets/data/content.ts
@@ -0,0 +1,58 @@
+import daveHoefler from "../images/dave-hoefler-okUIdo6NxGo-unsplash.jpg";
+import shermanYang from "../images/sherman-yang-VBBGigIuaDY-unsplash.jpg";
+import jakobOwens from "../images/jakob-owens-EwRM05V0VSI-unsplash.jpg";
+import findingDan from "../images/finding-dan-dan-grinwis-O35rT6OytRo-unsplash.jpg";
+import vincentiuSolomon from "../images/vincentiu-solomon-ln5drpv_ImI-unsplash.jpg";
+import silasBaisch from "../images/silas-baisch-Wn4ulyzVoD4-unsplash.jpg";
+import eugeneGolovesov from "../images/eugene-golovesov-EXdXp7Z4X4w-unsplash.jpg";
+import jenniferReynolds from "../images/jennifer-reynolds-_8HI5xf4TkE-unsplash.jpg";
+import kellenRiggin from "../images/kellen-riggin-SIBOiXKg0Ds-unsplash.jpg";
+import rafaelHoyos from "../images/rafael-hoyos-weht-zhkAp8DGkxw-unsplash.jpg";
+import sonyaRomanovska from "../images/sonya-romanovska-wzdXhyi3AiM-unsplash.jpg";
+
+export const content = [
+  {
+    image: daveHoefler,
+    caption: "Photo by Dave Hoefler on Unsplash",
+  },
+  {
+    image: shermanYang,
+    caption: "Photo by Sherman Yang n Unsplash",
+  },
+  {
+    image: jakobOwens,
+    caption: "Photo by Jakob Owens on Unsplash",
+  },
+  {
+    image: findingDan,
+    caption: "Photo by Dan Grinwis on Unsplash",
+  },
+  {
+    image: vincentiuSolomon,
+    caption: "Photo by Vincentiu Solomon on Unsplash",
+  },
+  {
+    image: silasBaisch,
+    caption: "Photo by Silas Baisch on Unsplash",
+  },
+  {
+    image: eugeneGolovesov,
+    caption: "Photo by Eugene Golovesov on Unsplash",
+  },
+  {
+    image: jenniferReynolds,
+    caption: "Photo by Jennifer reynolds on Unsplash",
+  },
+  {
+    image: kellenRiggin,
+    caption: "Photo by Kellen Riggin on Unsplash",
+  },
+  {
+    image: rafaelHoyos,
+    caption: "Photo by Rafael Hoyos on Unsplash",
+  },
+  {
+    image: sonyaRomanovska,
+    caption: "Photo by Sonya Romanovska on Unsplash",
+  },
+];
